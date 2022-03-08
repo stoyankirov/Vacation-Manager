d07465458c6b32f9da81732621fb15e62a56576d
gecho <cholakovge@gmail.com>
2022-03-08 12:32:43 +0200

    Cors configuration

--- Server/VacationManager/VacationManager.API/Startup.cs
+++ Server/VacationManager/VacationManager.API/Startup.cs
@@ -16,7 +16,6 @@ namespace VacationManager.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -25,9 +24,17 @@ namespace VacationManager.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VacationManager.API", Version = "v1" });
             });
+
+            // Cors configuration
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,6 +47,8 @@ namespace VacationManager.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
