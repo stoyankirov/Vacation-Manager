177e711d3f5aa14a2405db9fcc75af4cd17f16a2
gecho <cholakovge@gmail.com>
2022-03-22 16:28:41 +0200

    Global exception handling - returning 500 internal server error when unexpected error occurs

---
+++ Server/VacationManager/VacationManager.Domain/Models/ExceptionDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace VacationManager.Domain.Models
+{
+    using Newtonsoft.Json;
+
+    public class ExceptionDetails
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
